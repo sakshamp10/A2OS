25163c4d9a2782230753b220a5b744f0f2417c95
sakshamp10 <saksham21486@iiitd.ac.in>
2022-11-24 18:12:56 +0530

    1.1

--- cmake-build-debug/Q1_1.c
+++ cmake-build-debug/Q1_1.c
@@ -23,7 +23,7 @@ void* countA(){
     }
     struct timespec ta2;
     clock_gettime(CLOCK_REALTIME,&ta2);
-    printf("countA =%f\n",((ta2.tv_sec-ta1.tv_sec)+(ta2.tv_nsec-ta1.tv_nsec))/1e9);
+    printf("countA =%f\n",((ta1.tv_sec-ta2.tv_sec)+(ta1.tv_nsec-ta2.tv_nsec))/1e9);
     return NULL;
 }
 
@@ -40,7 +40,7 @@ void* countB(){
         }
     }
     clock_gettime(CLOCK_REALTIME,&tb2);
-    printf("countB =%f\n",(tb2.tv_sec-tb1.tv_sec+tb2.tv_nsec-tb1.tv_nsec)/1e9);
+    printf("countB =%f\n",(tb1.tv_sec-tb2.tv_sec+tb1.tv_nsec-tb2.tv_nsec)/1e9);
     return NULL;
 }
 
@@ -57,7 +57,7 @@ void* countC(){
         }
     }
     clock_gettime(CLOCK_REALTIME,&tc2);
-    printf("countC =%f\n",(tc2.tv_sec-tc1.tv_sec+tc2.tv_nsec-tc1.tv_nsec)/1e9);
+    printf("countC =%f\n",(tc1.tv_sec-tc2.tv_sec+tc1.tv_nsec-tc2.tv_nsec)/1e9);
     return NULL;
 }
 
