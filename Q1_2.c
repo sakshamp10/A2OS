f2657255de2e4669608437b3ef1df4bb5eb62198
sakshamp10 <saksham21486@iiitd.ac.in>
2022-11-24 20:33:05 +0530

    1.2 check 2

--- Q1_2.c
+++ Q1_2.c
@@ -1,73 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <pthread.h>
-#include <sched.h>
-#include <math.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include<unistd.h>
+#include <time.h>
+#include <sched.h>
 
-int main(){
-    pid_t pid1, pid2, pid3;
+int main() {
+    FILE *datafile = fopen("/home/saksham/histogram/data.txt", "w");
+    pid_t proc_a = fork();
+    if (proc_a == 0) {
+        struct timespec ta1, ta2;
+        int Ta1 = clock_gettime(CLOCK_REALTIME, &ta1);
+        pid_t proc_1 = fork();
+        struct sched_param sp1;
+        sp1.sched_priority = 0;
+        sched_setscheduler(proc_1, SCHED_OTHER, &sp1);
+        if (proc_1 == 0) {
+            execlp("/bin/sh", "sh", "b1.sh", NULL);
+        } else if (proc_1 > 0) {
+            int w1 = waitpid(proc_1, 0, 0);
+            int Tb2 = clock_gettime(CLOCK_REALTIME, &ta2);
+            double time = (ta2.tv_sec - ta1.tv_sec + (ta2.tv_nsec - ta1.tv_nsec) / (double) 1e9);
 
-    struct sched_param* schedp1 = (struct sched_param*)malloc(sizeof(struct sched_param));
-    if(schedp1!=NULL) {
-        schedp1->sched_priority=0;
-    }
-
-    struct sched_param* schedp2 = (struct sched_param*)malloc(sizeof(struct sched_param));
-    if(schedp2!=NULL) {
-        schedp2->sched_priority=1;
-    }
-
-    struct sched_param* schedp3 = (struct sched_param*)malloc(sizeof(struct sched_param));
-    if(schedp3!=NULL) {
-        schedp3->sched_priority=1;
-    }
-
-    struct timespec s1,s2,s3,e1,e2,e3;
-    int S1 = clock_gettime(CLOCK_REALTIME, &s1);
-    pid1 = fork();
-    int S2 = clock_gettime(CLOCK_REALTIME, &s2);
-    pid2 = fork();
-    int S3 = clock_gettime(CLOCK_REALTIME, &s3);
-    pid3 = fork();
-    //pid2 =pid1;
-    //pid3 = pid1;
-    if(pid1==0 && pid2!=0 && pid3!=0){
-        sched_setscheduler(pid1,SCHED_OTHER,schedp1);
-        execlp("/bin/bash","sh","bash1.sh",NULL);
-        exit(0);
-    }
-
-    else if(pid2==0 && pid1!=0 && pid3!=0){
-        sched_setscheduler(pid2,SCHED_RR,schedp2);
-        execlp("/bin/bash","sh","bash2.sh",NULL);
-        exit(0);
-    }
-    else if(pid3==0 && pid2!=0 & pid1!=0){
-        sched_setscheduler(pid3,SCHED_FIFO,schedp3);
-        execlp("/bin/bash","sh","bash3.sh",NULL);
-        exit(0);
-    }
-
-    for(int i =0; i<3; i++){
-        pid_t pz = wait(NULL);
-        if(pz == pid1){
-            int E1=clock_gettime(CLOCK_REALTIME,&e1);
-            double ans=(e1.tv_sec-s1.tv_sec)+(e1.tv_nsec-s1.tv_nsec)/1e9;
-            printf("----------RunTime for first thread: %lfs----------\n",(ans));
+            printf("----------RunTime for first thread: %lfs----------\n", (time));
+            fprintf(datafile, "%lf\n", time);
         }
-        else if(pz == pid2){
-            int E2=clock_gettime(CLOCK_REALTIME,&e2);
-            double ans2=e2.tv_sec-s2.tv_sec+(e2.tv_nsec-s2.tv_nsec)/1e9;
-            printf("----------RunTime for second thread: %lfs----------\n",(ans2));
+    } else {
+        pid_t proc_b = fork();
+        if (proc_b == 0) {
+            struct timespec tb1, tb2;
+            int Tb1 = clock_gettime(CLOCK_REALTIME, &tb1);
+            pid_t proc_2 = fork();
+            struct sched_param sp2;
+            sp2.sched_priority = 0;
+            sched_setscheduler(proc_2, SCHED_OTHER, &sp2);
+            if (proc_2 == 0) {
+                execlp("/bin/sh", "sh", "b2.sh", NULL);
+            } else if (proc_2 > 0) {
+                int w2 = waitpid(proc_2, 0, 0);
+                int Tb2 = clock_gettime(CLOCK_REALTIME, &tb2);
+                double time = (tb2.tv_sec - tb1.tv_sec + (tb2.tv_nsec - tb1.tv_nsec) / (double) 1e9);
+
+                printf("----------RunTime for first thread: %lfs----------\n", (time));
+                fprintf(datafile, "%lf\n", time);
+            }
+        } else {
+            pid_t proc_c = fork();
+            if (proc_c == 0) {
+                struct timespec tc1, tc2;
+                int Tc1 = clock_gettime(CLOCK_REALTIME, &tc1);
+                pid_t proc_3 = fork();
+                struct sched_param sp2;
+                sp2.sched_priority = 0;
+                sched_setscheduler(proc_3, SCHED_OTHER, &sp2);
+                if (proc_3 == 0) {
+                    execlp("/bin/sh", "sh", "b2.sh", NULL);
+                } else if (proc_3 > 0) {
+                    int w3 = waitpid(proc_3, 0, 0);
+                    int Tc2 = clock_gettime(CLOCK_REALTIME, &tc2);
+                    double time = (tc2.tv_sec - tc1.tv_sec + (tc2.tv_nsec - tc1.tv_nsec) / (double) 1e9);
+
+                    printf("----------RunTime for first thread: %lfs----------\n", (time));
+                    fprintf(datafile, "%lf\n", time);
+                }
+                wait(NULL);
+            }
+            wait(NULL);
         }
-        else if(pz == pid3){
-            int E3=clock_gettime(CLOCK_REALTIME,&e3);
-            double ans3=e3.tv_sec-s3.tv_sec+(e3.tv_nsec-s3.tv_nsec)/1e9;
-            printf("----------RunTime for third thread: %lfs----------\n",(ans3));
-        }
+        wait(NULL);
+        fclose(datafile);
+        return 0;
     }
 }
