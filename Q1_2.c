1cdc5a521ecf2fbc3c9468f64c8883cc1ec05025
sakshamp10 <saksham21486@iiitd.ac.in>
2022-11-24 21:11:18 +0530

    1.2 check 2

--- Q1_2.c
+++ Q1_2.c
@@ -18,7 +18,8 @@ int main() {
         sched_setscheduler(proc_1, SCHED_OTHER, &sp1);
         if (proc_1 == 0) {
             execlp("/bin/sh", "sh", "b1.sh", NULL);
-        } else if (proc_1 > 0) {
+        }
+        else if (proc_1 > 0) {
             int w1 = waitpid(proc_1, 0, 0);
             int Tb2 = clock_gettime(CLOCK_REALTIME, &ta2);
             double time = (ta2.tv_sec - ta1.tv_sec + (ta2.tv_nsec - ta1.tv_nsec) / (double) 1e9);
@@ -26,9 +27,9 @@ int main() {
             printf("----------RunTime for first thread: %lfs----------\n", (time));
             fprintf(datafile, "%lf\n", time);
         }
-    } else {
+
         pid_t proc_b = fork();
-        if (proc_b == 0) {
+        if(proc_b==0){
             struct timespec tb1, tb2;
             int Tb1 = clock_gettime(CLOCK_REALTIME, &tb1);
             pid_t proc_2 = fork();
@@ -45,15 +46,15 @@ int main() {
                 printf("----------RunTime for first thread: %lfs----------\n", (time));
                 fprintf(datafile, "%lf\n", time);
             }
-        } else {
+
             pid_t proc_c = fork();
-            if (proc_c == 0) {
+            if(proc_c==0){
                 struct timespec tc1, tc2;
                 int Tc1 = clock_gettime(CLOCK_REALTIME, &tc1);
                 pid_t proc_3 = fork();
-                struct sched_param sp2;
+                struct sched_param sp3;
                 sp2.sched_priority = 0;
-                sched_setscheduler(proc_3, SCHED_OTHER, &sp2);
+                sched_setscheduler(proc_3, SCHED_OTHER, &sp3);
                 if (proc_3 == 0) {
                     execlp("/bin/sh", "sh", "b2.sh", NULL);
                 } else if (proc_3 > 0) {
@@ -64,12 +65,61 @@ int main() {
                     printf("----------RunTime for first thread: %lfs----------\n", (time));
                     fprintf(datafile, "%lf\n", time);
                 }
+            }
+            else{
                 wait(NULL);
             }
+        }
+        else{
             wait(NULL);
         }
+    }
+    else {
         wait(NULL);
+    }
+        //     else {
+//        pid_t proc_b = fork();
+//        if (proc_b == 0) {
+////            struct timespec tb1, tb2;
+////            int Tb1 = clock_gettime(CLOCK_REALTIME, &tb1);
+////            pid_t proc_2 = fork();
+////            struct sched_param sp2;
+////            sp2.sched_priority = 0;
+////            sched_setscheduler(proc_2, SCHED_OTHER, &sp2);
+////            if (proc_2 == 0) {
+////                execlp("/bin/sh", "sh", "b2.sh", NULL);
+////            } else if (proc_2 > 0) {
+////                int w2 = waitpid(proc_2, 0, 0);
+////                int Tb2 = clock_gettime(CLOCK_REALTIME, &tb2);
+////                double time = (tb2.tv_sec - tb1.tv_sec + (tb2.tv_nsec - tb1.tv_nsec) / (double) 1e9);
+////
+////                printf("----------RunTime for first thread: %lfs----------\n", (time));
+////                fprintf(datafile, "%lf\n", time);
+////            }
+//        } else {
+//            pid_t proc_c = fork();
+//            if (proc_c == 0) {
+//                struct timespec tc1, tc2;
+//                int Tc1 = clock_gettime(CLOCK_REALTIME, &tc1);
+//                pid_t proc_3 = fork();
+//                struct sched_param sp2;
+//                sp2.sched_priority = 0;
+//                sched_setscheduler(proc_3, SCHED_OTHER, &sp2);
+//                if (proc_3 == 0) {
+//                    execlp("/bin/sh", "sh", "b2.sh", NULL);
+//                } else if (proc_3 > 0) {
+//                    int w3 = waitpid(proc_3, 0, 0);
+//                    int Tc2 = clock_gettime(CLOCK_REALTIME, &tc2);
+//                    double time = (tc2.tv_sec - tc1.tv_sec + (tc2.tv_nsec - tc1.tv_nsec) / (double) 1e9);
+//
+//                    printf("----------RunTime for first thread: %lfs----------\n", (time));
+//                    fprintf(datafile, "%lf\n", time);
+//                }
+//                wait(NULL);
+//            }
+//            wait(NULL);
+//        }
+//        wait(NULL);
         fclose(datafile);
         return 0;
-    }
 }
